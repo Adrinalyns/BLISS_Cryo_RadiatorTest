c5b1fa535f58a13994f4958781d45b8f043a7f1e
Adrinalyns <adrien.boissy22@gmail.com>
2026-04-07 12:12:03 -0400

    Adding file to read multiple RTD using MAX31865

---
+++ cryo1/RTD_1.py
@@ -0,0 +1,21 @@
+import board
+import digitalio
+import adafruit_max31865
+import time
+
+
+spi = board.SPI()
+cs1 = digitalio.DigitalInOut(board.D6)
+sensor1 = adafruit_max31865.MAX31865(spi,cs1,wires=4, rtd_nominal=100.01, ref_resistor=430)
+
+cs2 = digitalio.DigitalInOut(board.D12)
+sensor2 = adafruit_max31865.MAX31865(spi,cs2,wires=4, rtd_nominal=99.99, ref_resistor=430)
+
+while True:
+    print('Temperature 1: {0:0.3f}C'.format(sensor1.temperature))
+    print('Temperature 2: {0:0.3f}C'.format(sensor2.temperature))
+    print('Resistance 1: {0:0.3f} Ohms'.format(sensor1.resistance))
+    print('Resistance 2: {0:0.3f} Ohms'.format(sensor2.resistance))
+    time.sleep(1)
+
+
