05568242d4fc6d2c9b48dde4d674311fe68aa211
Adrien Boissy <adrienb@MacBook-Air-de-Adrien-7.local>
2026-04-14 11:09:37 -0400

    Changing the 0°C resistance for the RTDs reading to match the final setup

--- cryo1/main_loop_2.py
+++ cryo1/main_loop_2.py
@@ -32,13 +32,13 @@ cs_k = digitalio.DigitalInOut(board.D5)
 sensor_k = adafruit_max31855.MAX31855(spi, cs_k)
 
 cs1 = digitalio.DigitalInOut(board.D6)
-sensor1 = adafruit_max31865.MAX31865(spi, cs1, wires=4, rtd_nominal=100.01, ref_resistor=430)
+sensor1 = adafruit_max31865.MAX31865(spi, cs1, wires=4, rtd_nominal=100.02, ref_resistor=430)
 
 cs2 = digitalio.DigitalInOut(board.D12)
 sensor2 = adafruit_max31865.MAX31865(spi, cs2, wires=4, rtd_nominal=99.99, ref_resistor=430)
 
 cs3 = digitalio.DigitalInOut(board.D13)
-sensor3 = adafruit_max31865.MAX31865(spi, cs3, wires=4, rtd_nominal=100.02, ref_resistor=430)
+sensor3 = adafruit_max31865.MAX31865(spi, cs3, wires=4, rtd_nominal=100.01, ref_resistor=430)
 
 # --- Color logic (Type K only) ---
 def get_color(temp):
